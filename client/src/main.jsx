dcb788bc9e4582713ab9d20dafbc1bf9a46cc35c
Kartikey Tandon <tandonkartiey11@gmail.com>
2024-02-20 13:05:20 +0530

    added deployed api

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { SocketProvider } from './context/socketProvider.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { SocketProvider } from "./context/SocketProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <SocketProvider>
         <App />
       </SocketProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
